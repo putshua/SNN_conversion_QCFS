d04192adc4415e0e0786e1763f609966f17b6271
Andrea Pinto <andreakiro.pinto@gmail.com>
2022-05-26 19:01:12 +0200

    Live module with animated histogram

--- live.py
+++ live.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 from dv import NetworkFrameInput
 import numpy as np
 import torch
@@ -24,8 +27,30 @@ class LiveModule:
         self.model = model
         self.accumulator = accumulator
         self.device = device
+        self.x = np.arange(len(DVS_LABELS)) - 0.5
+        self.y = np.zeros(len(self.x))
 
-    def start(self, address = '127.0.0.1', port = 9466):
+    def start(self, interval = 500, address = '127.0.0.1', port = 9466):
+        self.load_animation(interval)
+        self.stream(address, port)
+
+    def load_animation(self, interval):
+        fig, ax = plt.subplots()
+        _, _, bar_container = ax.hist(self.x, self.y)
+        ax.set_ylim(top=50)  # tunable ? 
+        ax.xaxis.set_ticks(np.arange(len(DVS_LABELS) + 1))
+        ax.xaxis.set_ticklabels(np.arange(len(DVS_LABELS) + 1))
+        ani = animation.FuncAnimation(fig, self.live_hist(bar_container), interval=interval, blit=True)
+        plt.show()
+
+    def live_hist(self, bar_container):
+        def animate(frame_number):
+            for count, rect in zip(self.y, bar_container.patches):
+                rect.set_height(count)
+            return bar_container.patches
+        return animate
+
+    def stream(self, address, port):
         with NetworkFrameInput(address=address, port=port) as frames:
             idx = 0
             spikes_buffer = 0
@@ -33,25 +58,18 @@ class LiveModule:
             self.model = self.model.cuda(self.device)
 
             for frame in frames:
-
                 # create image
                 f = frame.image - 127.0
                 channel1 = np.clip(f, 0, 128) 
                 channel2 = np.clip(f, -127, 0)
                 image = np.concatenate([channel1, channel2], axis=2)
-                
+
+                # process image
                 image = torch.from_numpy(image)
                 image = image.unsqueeze(0)
                 image = torch.permute(image, (0, 3, 1, 2))
                 image = image[:, :, :128, :128]
-
-                # cv2.imshow('image test', image[0, 0, :, :].numpy())
-                # cv2.waitKey(100)
-                # print(image)
-
-                # print(image.numpy())
                 image = image.cuda()
-
 
                 # call model
                 with torch.no_grad():
@@ -63,25 +81,14 @@ class LiveModule:
                 if idx > self.accumulator:
                     spikes_buffer -= spikes_mem.pop(0)
                 
-                # translate pred
-
-                # ToDo: retrieve confidence distribution
-                # Question: what's happening here ?
-
+                # get prediction
                 pred = spikes_buffer[:, :10].max(1)[1]
+                self.y = spikes_buffer[:, :10].tolist()
 
-                # print pred
-                # ToDo: Showcase confidencence distribution
-                if idx%100 == 0: 
+                # display results
+                if idx % 100 == 0: 
                     os.system('clear')
-
                     print(spikes_buffer[:, :10])
                     print(DVS_LABELS[pred.item()])
-
-
-                # print live images 
-                # ToDo: elaborate something fancier
-                # ToDo: add a way to stop the simulation
-
             
                 idx += 1
